e548d193bccbb3a803326e39778b6d438b06c85d
Michael Zh <mikooomichael@gmail.com>
2025-05-05 17:11:16 -0400

    gradle: Version bump to 0.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dd3boh.outertune"
         minSdk = 26
         targetSdk = 35
-        versionCode = 52
-        versionName = "0.8.0"
+        versionCode = 53
+        versionName = "0.8.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
