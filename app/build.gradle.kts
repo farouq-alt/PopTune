19dba222c1bfb504078113875de773ceb674f87d
Michael Zh <mikooomichael@gmail.com>
2025-07-15 19:14:32 -0400

    gradle: Version bump to 0.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.dd3boh.outertune"
         minSdk = 24
         targetSdk = 36
-        versionCode = 62
-        versionName = "0.9.0"
+        versionCode = 63
+        versionName = "0.9.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
