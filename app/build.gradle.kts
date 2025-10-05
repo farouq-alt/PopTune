1bafe8cd3298dac90fc004fea14caec649250e84
Michael Zh <mikooomichael@gmail.com>
2025-10-05 13:23:13 -0400

    outertune: Version bump to 0.10.0 (for real this time)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.dd3boh.outertune"
         minSdk = 24
         targetSdk = 36
-        versionCode = 69
-        versionName = "0.10.0-b3"
+        versionCode = 70
+        versionName = "0.10.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
