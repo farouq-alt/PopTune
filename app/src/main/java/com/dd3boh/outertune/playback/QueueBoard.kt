348745897d90a37d27d45cf79afdcef7abf82912
Michael Zh <mikooomichael@gmail.com>
2025-10-16 11:06:12 -0400

    multiqueue: Use withlock in databaseDispatcher
    Fixes #901 I think

--- app/src/main/java/com/dd3boh/outertune/playback/QueueBoard.kt
+++ app/src/main/java/com/dd3boh/outertune/playback/QueueBoard.kt
@@ -34,6 +34,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.util.PriorityQueue
 import kotlin.math.max
 import kotlin.math.min
@@ -834,34 +835,34 @@ class QueueBoard(
     /**
      * Execute the most recent save request, with a 5 second delay from function call
      */
-    private fun databaseDispatcher() {
+    private suspend fun databaseDispatcher() {
         Log.d(TAG, "Starting database save task")
         if (jobActive.isLocked) {
             Log.d(TAG, "Database save task is already active, aborting")
             return
         }
 
-        jobActive.tryLock()
-        while (queueEntity.isNotEmpty() || queueSongMap.isNotEmpty()) {
-            runBlocking {
-                delay(5000L)
-            }
-            Log.d(TAG, "Running database save task")
+        jobActive.withLock {
+            while (queueEntity.isNotEmpty() || queueSongMap.isNotEmpty()) {
+                runBlocking {
+                    delay(5000L)
+                }
+                Log.d(TAG, "Running database save task")
 
-            // saving songs nukes the queue entity in the process, about it shouldn't matter since are same queue object
-            if (!queueSongMap.isEmpty()) {
-                queueSongMap.last().job.start()
-                queueSongMap.clear()
-                continue
-            }
+                // saving songs nukes the queue entity in the process, about it shouldn't matter since are same queue object
+                if (!queueSongMap.isEmpty()) {
+                    queueSongMap.last().job.start()
+                    queueSongMap.clear()
+                    continue
+                }
 
-            if (!queueEntity.isEmpty()) {
-                queueEntity.last().job.start()
-                queueEntity.clear()
-                continue
+                if (!queueEntity.isEmpty()) {
+                    queueEntity.last().job.start()
+                    queueEntity.clear()
+                    continue
+                }
             }
         }
-        jobActive.unlock()
         Log.d(TAG, "Exiting database save task")
     }
 
