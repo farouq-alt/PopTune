868d51fdba524f9faeac7eb8f933d2727c6c7931
Michael Zh <mikooomichael@gmail.com>
2025-11-01 01:43:57 -0400

    outertune: Version bump to 0.10.0-rc1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 70
-        versionName = "0.10.0-b7"
+        versionName = "0.10.0-rc1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
